022eb0b30acd9dcf9ff8aaa0891d6fd3836cad10
HarisHidayatul <Hidayatulloh.haris@gmail.com>
2023-02-25 21:47:49 +0700

    Menambahkan beberapa halaman pada bagian fitur

--- perusahaan/script.js
+++ perusahaan/script.js
@@ -37,9 +37,41 @@ function radioSelect(index) {
     var elementRadio = document.getElementsByName("radioSelect");
     for (var i = 0; i < elementRadio.length; i++) {
         if (i == index) {
-            elementRadio[i].classList.add('active');
+            elementRadio[i].classList.add("active");
         } else {
-            elementRadio[i].classList.remove('active');
+            elementRadio[i].classList.remove("active");
         }
     }
+}
+
+var startingX, movingX;
+var translatePosition = 0;
+
+function touchStart(evt) {
+    startingX = evt.touches[0].clientX;
+}
+
+function touchMove(evt) {
+    movingX = evt.touches[0].clientX;
+}
+
+function touchEnd() {
+    if (startingX + 100 < movingX) {
+        console.log("right");
+        translatePosition += 35;
+        if (translatePosition >= 0) {
+            translatePosition = 0;
+        }
+        console.log(translatePosition);
+        moveScroll();
+    } else if (startingX - 100 > movingX) {
+        console.log("left");
+        translatePosition -= 35;
+        moveScroll();
+    }
+}
+
+function moveScroll() {
+    document.getElementById('scrollProgram').style.transform = "translateX(" + translatePosition + "vw)";
+    document.getElementById('scrollProgram').style.transition = " all 2s";
 }
